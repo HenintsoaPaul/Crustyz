1ef0716f2cbaf409a1036628f08d0b9e774d55c5
Henintsoa Paul <henintsoapaul@gmail.com>
2024-12-21 08:41:15 +0300

    [setu]-Init spring boot

---
+++ src/main/java/mg/crustyz/CrustyzApplication.java
@@ -0,0 +1,13 @@
+package mg.crustyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrustyzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrustyzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/crustyz/CrustyzApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.crustyz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrustyzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
