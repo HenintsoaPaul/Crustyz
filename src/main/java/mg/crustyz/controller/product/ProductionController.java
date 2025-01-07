234c73985dbd293effc532b053b06998c946f584
Henintsoa Paul <henintsoapaul@gmail.com>
2025-01-07 19:01:44 +0300

    [production]-Filtre par ingredient

--- src/main/java/mg/crustyz/controller/product/ProductionController.java
+++ src/main/java/mg/crustyz/controller/product/ProductionController.java
@@ -1,13 +1,13 @@
 package mg.crustyz.controller.product;
 
 import lombok.RequiredArgsConstructor;
-import mg.crustyz.dto.search.ProductionSearch;
 import mg.crustyz.entity.product.Ingredient;
 import mg.crustyz.entity.product.Product;
 import mg.crustyz.entity.product.Production;
 import mg.crustyz.repository.IngredientRepository;
 import mg.crustyz.repository.product.ProductRepository;
 import mg.crustyz.repository.product.ProductionRepository;
+import mg.crustyz.service.product.ProductionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -21,14 +21,20 @@ public class ProductionController {
     private final ProductionRepository productionRepository;
     private final IngredientRepository ingredientRepository;
     private final ProductRepository productRepository;
+    private final ProductionService productionService;
 
     @GetMapping
-    public String getAll( Model model ) {
+    public String getAll( Model model, @RequestParam(required = false) List<Integer> selectedIngredients ) {
+        List<Production> productions = productionService.findAll();
         List<Ingredient> ingredients = ingredientRepository.findAll();
 
-        model.addAttribute( "productionsList", productionRepository.findAll() );
+        if (selectedIngredients != null && !selectedIngredients.isEmpty()) {
+            productions = productionService.filterByIngredients(selectedIngredients);
+        }
+
+        model.addAttribute( "productionsList", productions );
         model.addAttribute( "ingredientsList", ingredients );
-        model.addAttribute( "productionSearch", new ProductionSearch( ingredients ) );
+
         return "products/productions/index";
     }
 
@@ -36,7 +42,6 @@ public class ProductionController {
     public String gotoSave( Model model ) {
         model.addAttribute( "production", new Production() );
 
-        // liste des produits
         List<Product> products = productRepository.findAll();
         model.addAttribute( "productsList", products );
 
