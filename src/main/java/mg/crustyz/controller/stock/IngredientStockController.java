762705d59edbcdd2a9c0432e502877d9e1e22e15
Henintsoa Paul <henintsoapaul@gmail.com>
2025-01-16 07:02:33 -0800

    Filtrer les ingredients et les produits par nom

--- src/main/java/mg/crustyz/controller/stock/IngredientStockController.java
+++ src/main/java/mg/crustyz/controller/stock/IngredientStockController.java
@@ -3,30 +3,42 @@ package mg.crustyz.controller.stock;
 import lombok.RequiredArgsConstructor;
 import mg.crustyz.entity.stock.IngredientStock;
 import mg.crustyz.service.IngredientStockService;
+
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping( "/stocks/ingredients" )
+@RequestMapping("/stocks/ingredients")
 public class IngredientStockController {
     private final IngredientStockService ingredientStockService;
 
     @GetMapping
-    public String get( Model model ) {
-        model.addAttribute( "ingredientStocksList", ingredientStockService.findAll() );
+    public String get(Model model,
+            @RequestParam(required = false) String nomIngredient) {
+        List<IngredientStock> ingredientStocks = ingredientStockService.findAll();
+
+        if (nomIngredient != null && !nomIngredient.isEmpty()) {
+            List<IngredientStock> byName = ingredientStockService.findByIngredientNameContaining(nomIngredient);
+
+            ingredientStocks.retainAll(byName);
+        }
+
+        model.addAttribute("ingredientStocksList", ingredientStocks);
         return "stocks/ingredients/index";
     }
 
-    @GetMapping( "/{id}" )
-    public String detail( Model model, @PathVariable Integer id )
+    @GetMapping("/{id}")
+    public String detail(Model model, @PathVariable Integer id)
             throws Exception {
-        IngredientStock ingredientStock = ingredientStockService.findById( id );
+        IngredientStock ingredientStock = ingredientStockService.findById(id);
         int idIngredient = ingredientStock.getIngredient().getId();
 
-        model.addAttribute( "stock", ingredientStock );
-        model.addAttribute( "mvtList", ingredientStockService.findAllMvtByIdIngredient( idIngredient ) );
+        model.addAttribute("stock", ingredientStock);
+        model.addAttribute("mvtList", ingredientStockService.findAllMvtByIdIngredient(idIngredient));
         return "stocks/ingredients/mvt";
     }
 }
